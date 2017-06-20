ce641aba89f47a13031c2a839ed2f36c0acac8e3
Phillip viens <phillip.viens@gmail.com>
2017-06-20 15:31:55 -0700

    Create Lab1_AboutMe.java

---
+++ Lab1_AboutMe.java
@@ -0,0 +1,14 @@
+// Phillip Viens
+// AboutMe.java
+// Purpose: This is the sample progrma for Lab_1 and will print a
+//          few bits of information about me to the screen.
+
+public class AboutMe
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Name: Phillip or Phil Viens");
+      System.out.println("Favorite Color: Blue");
+      System.out.println("Place of Birth: Montreal, QB, Canada");
+   }
+}
