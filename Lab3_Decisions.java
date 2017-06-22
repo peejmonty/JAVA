c750e0c4191e2b8f240ea20e2ae5ec01219a34a0
Phillip viens <phillip.viens@gmail.com>
2017-06-22 11:18:41 -0700

    Create Lab3_Decisions.java
    Lab with If and else if statements.

---
+++ Lab3_Decisions.java
@@ -0,0 +1,59 @@
+// Phillip Viens
+// Decisions
+// Purpose: Take two integers and decide if integer2 is a factor of 
+//           integer1
+
+import java.util.Scanner;
+
+public class Decisions
+{
+   public static void main(Strings[] args)
+   {
+      int Int1;   // initializes integer1
+      int Int2;   // initializes integer2
+      
+      Scanner keyboard = new Scanner(System.in);
+      
+      System.out.println("Hello, Let's do some division!");
+      Systme.out.println();
+      
+      // Has the user input the first integer
+      System.out.println("Please enter integer1. ");
+      Int1 = keyboard.nextInt();
+      
+      // Has the user input the second integer
+      System.out.println("Please enter integer2. ");
+      Int2 = keyboard.nextInt();
+      
+      // Checks to see if either of the integers are equal to zero
+      if (Int1 == 0 || Int2 == 0)
+      {
+         System.out.println("Can't divide by zero!");
+      }
+      
+      // Checks to see if Int1 is less than Int2
+      else if (Int1 < Int2)
+      {
+         System.out.println(Int1 + " is not divisible by " + Int2 + ".");
+      }
+      
+      // Checks to see if the result is a factor.
+      else if (Int1 % Int2 !=0)
+      {
+         System.out.println(Int1 + " is not divisible by " + Int2 + ".");
+      }
+      
+      //If there is no remainder it divides Int1 by Int2 to output 
+      // the factor.
+      else if (Int1 % Int2 == 0)
+      {
+         int Int3 = Int1 / Int2;
+         
+         System.out.println("Given the first integer of " + Int1 + ",");
+         System.out.println("and the second integer of " + Int2 + ".");
+         System.out.println("You have a factor of " + Int3 + ".");
+      }
+      
+      System.out.println("Thanks for playing, Goodbye!");
+   }
+}
