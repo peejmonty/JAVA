26706f4a857fc371682d6cc2d4ba1cf0fff195f9
Phillip viens <phillip.viens@gmail.com>
2017-06-26 16:19:14 -0700

    Update Quiz3.java

--- Quiz3.java
+++ Quiz3.java
@@ -4,12 +4,15 @@ public class Quiz3
 {
    public static void main(String[] args)
    {  
+      final int RANGE_LO = 10;
+      final int RANGE_HI = 24;
+      
       // get num
       Scanner scanner = new Scanner(System.in);
       int num;
-      System.out.print("Enter a number between 10 and 24: ");
+      System.out.print("Enter a number between " + RANGE_LO + " and " + RANGE_HI +": ");
       num = scanner.nextInt();    
-      while (!(10 <= num && num <= 24))
+      while (!(RANGE_LO <= num && num <= RANGE_HI))
       {
          // ask again
          System.out.println("WRONG");
