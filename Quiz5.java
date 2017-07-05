b5900cf42389af877b78c53d96aeedf41eff4f11
Phillip viens <phillip.viens@gmail.com>
2017-07-05 16:31:51 -0700

    Create Quiz5.java

---
+++ Quiz5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Quiz5
+{
+   public static void main(String[] args)
+   {
+       printNum(1);
+       printNum(-5672.13e-03);
+       printNum(0.0);
+       
+   }
+   
+   public static void printNum(int number)
+   {
+      System.out.println("Number" + number);
+   }
