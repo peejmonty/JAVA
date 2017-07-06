a8ff707ac3342a107a01265988c71717bde7b757
Phillip viens <phillip.viens@gmail.com>
2017-07-06 12:37:14 -0700

    Create Lab6_PrettyFors.java

---
+++ Lab6_PrettyFors.java
@@ -0,0 +1,13 @@
+//Phillip Viens
+//PrettyFors
+//Purpose: To take a positive integer and to display several patterns
+// on the screen
+
+import java.util.Scanner
+
+public class PrettyFors
+{
+   public static void main(String[] args)
+   {
+      int num = input();
+      first(num)
